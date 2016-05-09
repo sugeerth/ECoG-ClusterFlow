fdbc44c19610045471d31020c3e239b7bc84b891
Sugeerth Murugesan <sugeerth@gmail.com>
2016-05-09 16:21:20 -0700

    added Functionalities

--- RunProjectMain2.py
+++ RunProjectMain2.py
@@ -3,8 +3,8 @@ import os,sys
 # os.environ["PYTHONPATH"] = "/Users/sugeerthmurugesan/LBLProjects/workingCopy/NIPS"
 # os.system("python /Users/sugeerthmurugesan/LBLProjects/workingCopy/NIPS/BrainViewer.py")
 
-os.environ["PYTHONPATH"] = "/Users/sugeerthmurugesan/LBLProjects/workingCopy/gitUptoDateRepo/ECoG-ClusterFlow"
-os.system("python /Users/sugeerthmurugesan/LBLProjects/workingCopy/gitUptoDateRepo/ECoG-ClusterFlow/BrainViewer.py")
+os.environ["PYTHONPATH"] = "/Users/sugeerthmurugesan/LBLProjects/workingCopy/gitUptoDateRepo/ECoG-ClusterFlow/src"
+os.system("python /Users/sugeerthmurugesan/LBLProjects/workingCopy/gitUptoDateRepo/ECoG-ClusterFlow/src/BrainViewer.py")
 
 # # PROFILING
 # os.system("pycallgraph graphviz -- /Users/sugeerthmurugesan/LBLProjects/workingCopy/brain-vis/BrainViewer.py")
