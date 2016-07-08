dac13a40afba5afbb43b05bbd30832b360e11929
Sugeerth Murugesan <sugeerth@gmail.com>
2016-07-08 16:35:00 -0700

    new changes in viz

--- src/Data/dataProcessing.py
+++ src/Data/dataProcessing.py
@@ -1,41 +1,16 @@
 
-from PySide import QtCore, QtGui
-import scipy.io
-import math
-import weakref
-import scipy
-from PIL import Image
+import json
+from pprint import pprint
 import numpy as np
 import pprint
 
-ElectrodeSignalDataName = 'sigData'
-ElectrodeConnectivityDataName = 'conDat'
-
-# Changes for RealData
-Connectvity_filename ='/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerEpilepsyData/enhancedConData.mat'
+"""
+This is the data that needs to be changed based on the format of the data
+"""
+Network_File='/Users/sugeerthmurugesan/ProjectRepos/OrthoProject/GraphData/karate.json'
 
 class dataProcessing(object):
-	    def __init__(self, Brain_image_filename,Electrode_ElectrodeData_filename,Electrode_mat_filename,ElectrodeSignals):
-			self.im = Image.open(Brain_image_filename)
-			self.syllableUnit = 0 
-			self.Timestep =0
+    def __init__(self):
+		with open(Network_File) as data_file:    
+	    	self.data = json.load(data_file)
 
-			self.ElectrodeSignals = scipy.io.loadmat(ElectrodeSignals)
-			Data=scipy.io.loadmat(Electrode_ElectrodeData_filename)
-
-			self.mat = scipy.io.loadmat(Electrode_mat_filename)
-
-			temp = Data['electrode']
-			self.ElectrodeIds = temp[0]
-			self.ElectodeData = Data['C']
-
-			self.syllable, self.timestep, self.N , self.N  = np.shape(self.ElectodeData)
-			self.timestep = self.timestep - 1
-
-			""" The variables names for the new connecivity matrices, 
-			C == correlation matrix 
-			syllable == 6 syllables
-
-			time = mapping between electrodes
-			electrode == 58 electrodes  
-			"""
