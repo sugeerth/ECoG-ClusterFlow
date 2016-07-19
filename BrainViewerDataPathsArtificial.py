b177e9c169c911fd777f25ff9cb6e08689b23a30
Sugeerth Murugesan <sugeerth@gmail.com>
2016-07-18 20:46:05 -0700

    Changes

--- BrainViewerDataPathsArtificial.py
+++ BrainViewerDataPathsArtificial.py
@@ -1,6 +1,7 @@
 Electrode_mat_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/KrisDataset/EC2.reg_grd.mat'
 Brain_image_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/KrisDataset/EC2.brain_redo1.jpg'
 ElectrodeSignals ='/Users/sugeerthmurugesan/LBLProjects/workingCopy/brain-visWorkingRealData/SyntheticGeneratedData/SyntheticElectric.mat'
+# Electrode_ElectrodeData_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/KrisDataset/Bolasso_output_algnd.mat' 
 Electrode_ElectrodeData_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/KrisDataset/Bolasso_output_algnd.mat' 
 
 #set the flag data around here for convenience
