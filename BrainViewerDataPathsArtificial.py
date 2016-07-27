3f45b0e30a75a685b67f3e34aab3f1c44f84aa67
Sugeerth Murugesan <sugeerth@gmail.com>
2016-07-27 11:08:42 -0700

    Changed the path

--- BrainViewerDataPathsArtificial.py
+++ BrainViewerDataPathsArtificial.py
@@ -1,6 +1,13 @@
-Electrode_mat_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/KrisDataset/EC2.reg_grd.mat'
-Brain_image_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/KrisDataset/EC2.brain_redo1.jpg'
-ElectrodeSignals ='/Users/sugeerthmurugesan/LBLProjects/workingCopy/brain-visWorkingRealData/SyntheticGeneratedData/SyntheticElectric.mat'
+# Artificial Data 
+# Electrode_mat_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/KrisDataset/EC2.reg_grd.mat'
+# Brain_image_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/KrisDataset/EC2.brain_redo1.jpg'
+# ElectrodeSignals ='/Users/sugeerthmurugesan/LBLProjects/workingCopy/brain-visWorkingRealData/SyntheticGeneratedData/SyntheticElectric.mat'
+# Electrode_ElectrodeData_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/KrisDataset/Bolasso_output_algnd.mat' 
+
+#Real dataset to be visualzed 
+Electrode_mat_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerEpilepsyData/regData.mat'
+Brain_image_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerEpilepsyData/ec58_blank_clean_enhanced.png'
+ElectrodeSignals = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/SummerEpilepsyData/enhancedSigData.mat'
 Electrode_ElectrodeData_filename = '/Users/sugeerthmurugesan/LBLProjects/ELectrode/SummerProject/KrisDataset/Bolasso_output_algnd.mat' 
 
 #set the flag data around here for convenience
